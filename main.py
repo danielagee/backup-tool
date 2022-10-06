66d1ec219771e7446e40b6826c6f74f9fe0ccd9f
danielagee <danielhouston15@yahoo.com>
2022-10-06 23:26:47 +0300

    Day 18 of #100daysofcoding #Python. Search, extract, and write file list from multiple user defined directories.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-# Day 17 of #100daysofcoding #python.
-# Task 3 - Ask for user input on search location
+# Day 18 of #100daysofcoding #python.
+# Task 3 - Collect data from multiple user input folders.
 
 import glob
 
 dir_path = r'..\**\*.*'
-# Ask for top node folder
-root_dir = input('What is the path to search?\n'
+# Ask for top node folder of primary drive
+root_dir_active = input('What is the path to search?\n'
                    'Example format: c:\\MyFolder\\SearchHere\\ \n')
-root_dir += '**'
+root_dir_active += '**'
 
-for files in glob.glob(dir_path, root_dir=root_dir, recursive=True):
-    print(files)
+# Ask for top node folder of backup drive
+root_dir_backup = input('What is the path of your backup?\n')
+root_dir_backup += '**'
+
+for files_active in glob.glob(dir_path, root_dir=root_dir_active, recursive=True):
+    print(files_active)
+print('')
+for files_backup in glob.glob(dir_path, root_dir=root_dir_backup, recursive=True):
+    print(files_backup)
