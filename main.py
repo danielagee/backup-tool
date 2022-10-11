bb80b6bfdf1b772120891ac3a9e33c3f2f0c3264
danielagee <danielhouston15@yahoo.com>
2022-10-11 10:39:36 +0300

    Day 19 of #100daysofcoding #Python. Convert the lists of files to sets, sort the sets, and compare the results to see what's missing.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Day 18 Task 3 - Collect both directories
+# Day 18 Task 3 - Collect both directories
 
 import glob
 
@@ -7,7 +7,7 @@ dir_path = r'..\**\*.*'
 
 # Ask for top node folder, a.k.a. the root directory to search.
 root_dir_active = input('What is the path to search?\n'
-                   'Example format: c:\\MyFolder\\SearchHere\\ \n')
+                        'Example format: c:\\MyFolder\\SearchHere\\ \n')
 # Adds '**' to the user specified root directory so that Python will only search the root and subdirectories.
 root_dir_active += '**'
 
@@ -19,13 +19,34 @@ file_list_active = glob.glob(dir_path, root_dir=root_dir_active, recursive=True)
 
 file_list_backup = glob.glob(dir_path, root_dir=root_dir_backup, recursive=True)
 
+"""
+
 print('These files are present in your active drive.')
 for files_active in glob.glob(dir_path, root_dir=root_dir_active, recursive=True):
     print(files_active)
 
-# Adds a blank line between the results so we can see where the list changes.
+# Adds a blank line between the results to show where the list changes.
 print('')
 
 print('These files are present in the backup drive.')
 for files_backup in glob.glob(dir_path, root_dir=root_dir_backup, recursive=True):
     print(files_backup)
+
+"""
+
+# Creates sets from the file lists so they can be sorted and compared.
+set_active = set(file_list_active)
+sorted_list_active = sorted(set_active)
+set_backup = set(file_list_backup)
+sorted_list_backup = sorted(set_backup)
+
+# Subtracts the backup set from the active set to show what's in active and not in backup.
+set_missing = set_active - set_backup
+print('These files are missing from the backup:')
+print(set_missing)
+print('')
+
+# Subtracts the active set from the backup set to show what's in backup and not in active.
+print('These files are in backup but missing from the active directory:')
+set_extra = set_backup - set_active
+print(set_extra)
