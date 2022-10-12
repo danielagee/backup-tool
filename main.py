fb74dc80feb68ad448ac4b2e10aaaf2915c505d8
danielagee <danielhouston15@yahoo.com>
2022-10-12 10:29:35 +0300

    Day 20 of #100daysofcoding #Python. Print the list of missing and extra backup files line by line rather than bundled into a single line.

--- main.py
+++ main.py
@@ -19,22 +19,18 @@ root_dir_backup += '**'
 file_list_active = glob.glob(dir_path, root_dir=root_dir_active, recursive=True)
 file_list_backup = glob.glob(dir_path, root_dir=root_dir_backup, recursive=True)
 
-"""
-
-print('These files are present in your active drive.')
-for files_active in glob.glob(dir_path, root_dir=root_dir_active, recursive=True):
-    print(files_active)
+#print('These files are present in your active drive.')
+#for files_active in glob.glob(dir_path, root_dir=root_dir_active, recursive=True):
+#    print(files_active)
 
 # Adds a blank line between the results to show where the list changes.
-print('')
+#print('')
 
-print('These files are present in the backup drive.')
-for files_backup in glob.glob(dir_path, root_dir=root_dir_backup, recursive=True):
-    print(files_backup)
+#print('These files are present in the backup drive.')
+#for files_backup in glob.glob(dir_path, root_dir=root_dir_backup, recursive=True):
+#    print(files_backup)
 
-"""
-
-# Creates sets from the file lists so they can be sorted and compared.
+# Creates sets from the file lists for better comparison (order does not matter).
 set_active = set(file_list_active)
 #sorted_list_active = sorted(set_active)
 set_backup = set(file_list_backup)
@@ -43,10 +39,16 @@ set_backup = set(file_list_backup)
 # Subtracts the backup set from the active set to show what's in active and not in backup.
 set_missing = set_active - set_backup
 print('These files are missing from the backup:')
-print(set_missing)
+#print(set_missing)
+# Prints a parsed list of missing files line by line rather than in a single line.
+for item in set_missing:
+    print(item)
 print('')
 
 # Subtracts the active set from the backup set to show what's in backup and not in active.
 print('These files are in backup but missing from the active directory:')
 set_extra = set_backup - set_active
-print(set_extra)
+#print(set_extra)
+# Prints a parsed list of extra files line by line rather than in a single line.
+for item in set_extra:
+    print(item)
