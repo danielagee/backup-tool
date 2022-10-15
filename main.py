62b98a8fb5ec9b1b99c72b6b4e08ddcbe69885a4
danielagee <danielhouston15@yahoo.com>
2022-10-15 07:23:38 +0300

    Day 23 of #100daysofcoding #Python. Writes the sorted list of missing and extra files to two separate text files for easy usage.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Day 18 Task 3 - Collect both directories
+# Day 23
 
 import glob
 
@@ -8,6 +8,10 @@ dir_path = r'..\**\*.*'
 # Ask for top node folder, a.k.a. the root directory to search.
 root_dir_active = input('What is the path to search?\n'
                         'Example format: c:\\MyFolder\\SearchHere\\ \n')
+
+# Stores the name of the directory to write the final output files.
+write_dir = root_dir_active
+
 # Adds '**' to the user specified root directory so that Python will only search the root and subdirectories.
 root_dir_active += '**'
 
@@ -16,39 +20,38 @@ root_dir_backup = input('What is the path of your backup?\n')
 root_dir_backup += '**'
 
 # Extracts the active and backup file lists from the user specified folders.
+print('Extracting files from ' + root_dir_active)
 file_list_active = glob.glob(dir_path, root_dir=root_dir_active, recursive=True)
+print('Extracting files from ' + root_dir_backup)
 file_list_backup = glob.glob(dir_path, root_dir=root_dir_backup, recursive=True)
 
-#print('These files are present in your active drive.')
-#for files_active in glob.glob(dir_path, root_dir=root_dir_active, recursive=True):
-#    print(files_active)
-
-# Adds a blank line between the results to show where the list changes.
-#print('')
-
-#print('These files are present in the backup drive.')
-#for files_backup in glob.glob(dir_path, root_dir=root_dir_backup, recursive=True):
-#    print(files_backup)
-
 # Creates sets from the file lists for better comparison (order does not matter).
+# print('Lists extracted. Creating Sets...')
 set_active = set(file_list_active)
-#sorted_list_active = sorted(set_active)
 set_backup = set(file_list_backup)
-#sorted_list_backup = sorted(set_backup)
 
-# Subtracts the backup set from the active set to show what's in active and not in backup.
-set_missing = set_active - set_backup
-print('These files are missing from the backup:')
-#print(set_missing)
-# Prints a parsed list of missing files line by line rather than in a single line.
+# Subtracts the backup set from the active set to show what's in the active and not in the backup, then sorts the result.
+print('Sets created. Sorting sets...')
+set_missing = sorted(set_active - set_backup)
+
+# Create a file to accept the missing backup list.
+print('Sets sorted. Creating missing backup list...')
+missing_backup = open(write_dir+'files_missing_in_backup.txt', 'w')
+
+# For loop to write a parsed list of all missing backup files to the text document.
+print('Missing backup list created. Writing missing backup file...')
 for item in set_missing:
-    print(item)
-print('')
+    missing_backup.write(str(item))
+    missing_backup.write('\n')
 
-# Subtracts the active set from the backup set to show what's in backup and not in active.
-print('These files are in backup but missing from the active directory:')
-set_extra = set_backup - set_active
-#print(set_extra)
-# Prints a parsed list of extra files line by line rather than in a single line.
+# Closes the text file holding the missing backup files.
+missing_backup.close()
+
+# Same process as above, but subtracts the active set from the backup set to show what's in backup and not in active.
+print('Missing backup list written. Creating extra files list...')
+set_extra = sorted(set_backup - set_active)
+missing_active = open(write_dir+'extra_files_in_backup.txt', 'w')
 for item in set_extra:
-    print(item)
+    missing_active.write(str(item))
+    missing_active.write('\n')
+missing_active.close()
